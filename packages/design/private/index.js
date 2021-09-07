f72b40bfa295752b63a6ba7b35ad05e454b400d2
Marek Kobida <marek.kobida@gmail.com>
2021-09-07 10:47:35 +0200

    update

--- packages/design/private/index.js
+++ packages/design/private/index.js
@@ -6,51 +6,42 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const allowedJSXAttributes_1 = __importDefault(require("./allowedJSXAttributes"));
-const package_json_1 = __importDefault(require("../package.json"));
-function default_1({ types: t }) {
-    return {
-        visitor: {
-            JSXOpeningElement(path) {
-                const attributes = [];
-                const className = [];
-                path.node.attributes.forEach(attribute => {
-                    if (t.isJSXAttribute(attribute))
-                        if (t.isJSXIdentifier(attribute.name)) {
-                            if (attribute.name.name === 'className') {
-                                if (t.isJSXExpressionContainer(attribute.value))
-                                    if (t.isExpression(attribute.value.expression))
-                                        return className.push(attribute.value.expression);
-                                if (t.isStringLiteral(attribute.value))
-                                    return className.push(attribute.value);
-                            }
-                            if (attribute.name.name in allowedJSXAttributes_1.default) {
-                                if (t.isJSXExpressionContainer(attribute.value))
-                                    if (t.isExpression(attribute.value.expression))
-                                        return className.push(t.callExpression(t.identifier('decodeResponsiveClassName'), [
-                                            t.stringLiteral(allowedJSXAttributes_1.default[attribute.name.name]),
-                                            attribute.value.expression,
-                                        ]));
-                                if (t.isStringLiteral(attribute.value))
-                                    return className.push(t.callExpression(t.identifier('decodeResponsiveClassName'), [
-                                        t.stringLiteral(allowedJSXAttributes_1.default[attribute.name.name]),
-                                        attribute.value,
-                                    ]));
-                            }
-                        }
-                    attributes.push(attribute);
-                });
-                if (className.length)
-                    attributes.push(t.jsxAttribute(t.jsxIdentifier('className'), t.jsxExpressionContainer(t.callExpression(t.identifier('decodeClassName'), [t.arrayExpression(className)]))));
-                path.node.attributes = attributes;
-            },
-            Program(path) {
-                path.unshiftContainer('body', [
-                    t.importDeclaration([t.importDefaultSpecifier(t.identifier('decodeClassName'))], t.stringLiteral(`${package_json_1.default.name}/private/helpers/decodeClassName`)),
-                    t.importDeclaration([t.importDefaultSpecifier(t.identifier('decodeResponsiveClassName'))], t.stringLiteral(`${package_json_1.default.name}/private/helpers/decodeResponsiveClassName`)),
-                ]);
-            },
-        },
-    };
+const forBreakpoints_1 = __importDefault(require("./forBreakpoints"));
+const sizes_1 = __importDefault(require("./sizes"));
+const toString_1 = __importDefault(require("./toString"));
+function spacing() {
+    const i = (_) => `${_} !important`;
+    return (0, forBreakpoints_1.default)(b => {
+        function _1(p) {
+            return sizes_1.default.reduce((_, { name, size }) => ({
+                ..._,
+                [`.${b}${p[0]}-${name}`]: { [p]: i(size) },
+                [`.${b}${p[0]}-b-${name},.${b}${p[0]}-y-${name}`]: { [`${p}Bottom`]: i(size) },
+                [`.${b}${p[0]}-l-${name},.${b}${p[0]}-x-${name}`]: { [`${p}Left`]: i(size) },
+                [`.${b}${p[0]}-r-${name},.${b}${p[0]}-x-${name}`]: { [`${p}Right`]: i(size) },
+                [`.${b}${p[0]}-t-${name},.${b}${p[0]}-y-${name}`]: { [`${p}Top`]: i(size) },
+            }), {});
+        }
+        const _2 = sizes_1.default.reduce((_, { name, size }) => size === '0'
+            ? _
+            : {
+                ..._,
+                [`.${b}m-\\!${name}`]: { margin: i(`-${size}`) },
+                [`.${b}m-b-\\!${name},.${b}m-y-\\!${name}`]: { marginBottom: i(`-${size}`) },
+                [`.${b}m-l-\\!${name},.${b}m-x-\\!${name}`]: { marginLeft: i(`-${size}`) },
+                [`.${b}m-r-\\!${name},.${b}m-x-\\!${name}`]: { marginRight: i(`-${size}`) },
+                [`.${b}m-t-\\!${name},.${b}m-y-\\!${name}`]: { marginTop: i(`-${size}`) },
+            }, {});
+        return {
+            [`.${b}m-auto`]: { margin: i('auto') },
+            [`.${b}m-b-auto,.${b}m-y-auto`]: { marginBottom: i('auto') },
+            [`.${b}m-l-auto,.${b}m-x-auto`]: { marginLeft: i('auto') },
+            [`.${b}m-r-auto,.${b}m-x-auto`]: { marginRight: i('auto') },
+            [`.${b}m-t-auto,.${b}m-y-auto`]: { marginTop: i('auto') },
+            ..._1('margin'),
+            ..._1('padding'),
+            ..._2,
+        };
+    });
 }
-exports.default = default_1;
+console.log((0, toString_1.default)(spacing()));
--- packages/design/private/index.ts
+++ packages/design/private/index.ts
@@ -2,74 +2,54 @@
  * Copyright 2021 Marek Kobida
  */
 
-import * as babel from '@babel/core';
-import allowedJSXAttributes from './allowedJSXAttributes';
-import json from '../package.json';
+import forBreakpoints, { CSS } from './forBreakpoints';
+import sizes from './sizes';
+import toString from './toString';
 
-export default function ({ types: t }: typeof babel): babel.PluginObj {
-  return {
-    visitor: {
-      JSXOpeningElement(path) {
-        const attributes: (babel.types.JSXAttribute | babel.types.JSXSpreadAttribute)[] = [];
-        const className: babel.types.Expression[] = [];
+function spacing(): CSS {
+  const i = (_: string): string => `${_} !important`;
 
-        path.node.attributes.forEach(attribute => {
-          if (t.isJSXAttribute(attribute))
-            if (t.isJSXIdentifier(attribute.name)) {
-              if (attribute.name.name === 'className') {
-                if (t.isJSXExpressionContainer(attribute.value))
-                  if (t.isExpression(attribute.value.expression)) return className.push(attribute.value.expression);
+  return forBreakpoints(b => {
+    function _1(p: 'margin' | 'padding'): CSS {
+      return sizes.reduce(
+        (_, { name, size }) => ({
+          ..._,
+          [`.${b}${p[0]}-${name}`]: { [p]: i(size) },
+          [`.${b}${p[0]}-b-${name},.${b}${p[0]}-y-${name}`]: { [`${p}Bottom`]: i(size) },
+          [`.${b}${p[0]}-l-${name},.${b}${p[0]}-x-${name}`]: { [`${p}Left`]: i(size) },
+          [`.${b}${p[0]}-r-${name},.${b}${p[0]}-x-${name}`]: { [`${p}Right`]: i(size) },
+          [`.${b}${p[0]}-t-${name},.${b}${p[0]}-y-${name}`]: { [`${p}Top`]: i(size) },
+        }),
+        {}
+      );
+    }
 
-                if (t.isStringLiteral(attribute.value)) return className.push(attribute.value);
-              }
+    const _2: CSS = sizes.reduce(
+      (_, { name, size }) =>
+        size === '0'
+          ? _
+          : {
+              ..._,
+              [`.${b}m-\\!${name}`]: { margin: i(`-${size}`) },
+              [`.${b}m-b-\\!${name},.${b}m-y-\\!${name}`]: { marginBottom: i(`-${size}`) },
+              [`.${b}m-l-\\!${name},.${b}m-x-\\!${name}`]: { marginLeft: i(`-${size}`) },
+              [`.${b}m-r-\\!${name},.${b}m-x-\\!${name}`]: { marginRight: i(`-${size}`) },
+              [`.${b}m-t-\\!${name},.${b}m-y-\\!${name}`]: { marginTop: i(`-${size}`) },
+            },
+      {}
+    );
 
-              if (attribute.name.name in allowedJSXAttributes) {
-                if (t.isJSXExpressionContainer(attribute.value))
-                  if (t.isExpression(attribute.value.expression))
-                    return className.push(
-                      t.callExpression(t.identifier('decodeResponsiveClassName'), [
-                        t.stringLiteral(allowedJSXAttributes[attribute.name.name]),
-                        attribute.value.expression,
-                      ])
-                    );
-
-                if (t.isStringLiteral(attribute.value))
-                  return className.push(
-                    t.callExpression(t.identifier('decodeResponsiveClassName'), [
-                      t.stringLiteral(allowedJSXAttributes[attribute.name.name]),
-                      attribute.value,
-                    ])
-                  );
-              }
-            }
-
-          attributes.push(attribute);
-        });
-
-        if (className.length)
-          attributes.push(
-            t.jsxAttribute(
-              t.jsxIdentifier('className'),
-              t.jsxExpressionContainer(
-                t.callExpression(t.identifier('decodeClassName'), [t.arrayExpression(className)])
-              )
-            )
-          );
-
-        path.node.attributes = attributes;
-      },
-      Program(path) {
-        path.unshiftContainer('body', [
-          t.importDeclaration(
-            [t.importDefaultSpecifier(t.identifier('decodeClassName'))],
-            t.stringLiteral(`${json.name}/private/helpers/decodeClassName`)
-          ),
-          t.importDeclaration(
-            [t.importDefaultSpecifier(t.identifier('decodeResponsiveClassName'))],
-            t.stringLiteral(`${json.name}/private/helpers/decodeResponsiveClassName`)
-          ),
-        ]);
-      },
-    },
-  };
+    return {
+      [`.${b}m-auto`]: { margin: i('auto') },
+      [`.${b}m-b-auto,.${b}m-y-auto`]: { marginBottom: i('auto') },
+      [`.${b}m-l-auto,.${b}m-x-auto`]: { marginLeft: i('auto') },
+      [`.${b}m-r-auto,.${b}m-x-auto`]: { marginRight: i('auto') },
+      [`.${b}m-t-auto,.${b}m-y-auto`]: { marginTop: i('auto') },
+      ..._1('margin'),
+      ..._1('padding'),
+      ..._2,
+    };
+  });
 }
+
+console.log(toString(spacing()));
