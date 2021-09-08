d083dfe9981995148848d11d99f8a264a217fe0b
Marek Kobida <marek.kobida@gmail.com>
2021-09-08 14:14:26 +0200

    update

--- packages/design/private/index.js
+++ packages/design/private/index.js
@@ -6,7 +6,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const types_1 = require("../../types");
+const types_1 = require("../../types/types");
 const forBreakpoints_1 = __importDefault(require("./forBreakpoints"));
 const sizes_1 = __importDefault(require("./sizes"));
 const toString_1 = __importDefault(require("./toString"));
