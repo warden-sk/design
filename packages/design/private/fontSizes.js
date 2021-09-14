e0019f7580ca4f0ddece3ce64cce515bc1822261
Marek Kobida <marek.kobida@gmail.com>
2021-09-14 13:20:45 +0200

    up

--- packages/design/private/fontSizes.js
+++ packages/design/private/fontSizes.js
@@ -4,11 +4,17 @@
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 const fontSizes = [
-    ['1', '0.75rem'],
-    ['2', '1rem'],
+    ['\\-2', '0.75rem'],
+    ['\\-1', '0.875rem'],
+    ['1', '1rem'],
+    ['2', '1.125rem'],
     ['3', '1.25rem'],
     ['4', '1.5rem'],
     ['5', '1.75rem'],
     ['6', '2rem'],
+    ['7', '2.25rem'],
+    ['8', '2.5rem'],
+    ['9', '2.75rem'],
+    ['10', '3rem'], //       48px
 ];
 exports.default = fontSizes;
