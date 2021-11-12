66e55a620381bd94273ec0a6726c8d7b63cae336
Marek Kobida <marek.kobida@gmail.com>
2021-11-12 09:14:04 +0100

    update

--- packages/babel-plugin/index.js
+++ packages/babel-plugin/index.js
@@ -31,45 +31,47 @@ function default_1() {
     return {
         visitor: {
             JSXOpeningElement(path) {
-                const attributes = [];
-                const className = [];
-                path.node.attributes.forEach(attribute => {
-                    if (t.isJSXAttribute(attribute)) {
-                        if (t.isJSXIdentifier(attribute.name)) {
-                            if (attribute.name.name === 'className') {
-                                if (t.isJSXExpressionContainer(attribute.value)) {
-                                    if (t.isExpression(attribute.value.expression)) {
-                                        return className.push(attribute.value.expression);
+                if (t.isJSXIdentifier(path.node.name) && path.node.name.name !== 'svg') {
+                    const attributes = [];
+                    const className = [];
+                    path.node.attributes.forEach(attribute => {
+                        if (t.isJSXAttribute(attribute)) {
+                            if (t.isJSXIdentifier(attribute.name)) {
+                                if (attribute.name.name === 'className') {
+                                    if (t.isJSXExpressionContainer(attribute.value)) {
+                                        if (t.isExpression(attribute.value.expression)) {
+                                            return className.push(attribute.value.expression);
+                                        }
+                                    }
+                                    if (t.isStringLiteral(attribute.value)) {
+                                        return className.push(attribute.value);
                                     }
                                 }
-                                if (t.isStringLiteral(attribute.value)) {
-                                    return className.push(attribute.value);
-                                }
-                            }
-                            if (attribute.name.name in allowedJSXAttributes_1.default) {
-                                if (t.isJSXExpressionContainer(attribute.value)) {
-                                    if (t.isExpression(attribute.value.expression)) {
+                                if (attribute.name.name in allowedJSXAttributes_1.default) {
+                                    if (t.isJSXExpressionContainer(attribute.value)) {
+                                        if (t.isExpression(attribute.value.expression)) {
+                                            return className.push(t.callExpression(t.identifier('decodeResponsiveClassName'), [
+                                                t.stringLiteral(allowedJSXAttributes_1.default[attribute.name.name]),
+                                                attribute.value.expression,
+                                            ]));
+                                        }
+                                    }
+                                    if (t.isStringLiteral(attribute.value)) {
                                         return className.push(t.callExpression(t.identifier('decodeResponsiveClassName'), [
                                             t.stringLiteral(allowedJSXAttributes_1.default[attribute.name.name]),
-                                            attribute.value.expression,
+                                            attribute.value,
                                         ]));
                                     }
                                 }
-                                if (t.isStringLiteral(attribute.value)) {
-                                    return className.push(t.callExpression(t.identifier('decodeResponsiveClassName'), [
-                                        t.stringLiteral(allowedJSXAttributes_1.default[attribute.name.name]),
-                                        attribute.value,
-                                    ]));
-                                }
                             }
                         }
+                        attributes.push(attribute);
+                    });
+                    if (className.length) {
+                        attributes.push(t.jsxAttribute(t.jsxIdentifier('className'), t.jsxExpressionContainer(t.callExpression(t.identifier('decodeClassName'), [t.arrayExpression(className)]))));
                     }
-                    attributes.push(attribute);
-                });
-                if (className.length) {
-                    attributes.push(t.jsxAttribute(t.jsxIdentifier('className'), t.jsxExpressionContainer(t.callExpression(t.identifier('decodeClassName'), [t.arrayExpression(className)]))));
+                    path.node.attributes = attributes;
                 }
-                path.node.attributes = attributes;
             },
             Program(path) {
                 path.unshiftContainer('body', [
