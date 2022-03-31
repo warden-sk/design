796fa02caa7ea9d4f04ad21d4b41a6c909735183
Marek Kobida <marek.kobida@gmail.com>
2022-03-31 16:17:05 +0200

    update

--- packages/babel-plugin/index.ts
+++ packages/babel-plugin/index.ts
@@ -2,87 +2,85 @@
  * Copyright 2022 Marek Kobida
  */
 
-import * as babel from '@babel/core';
 import * as t from '@babel/types';
+import { Visitor } from '@babel/traverse';
 import allowedJSXAttributes from './private/allowedJSXAttributes';
 
-export default function (): babel.PluginItem {
-  return {
-    visitor: {
-      JSXOpeningElement(path) {
-        if (t.isJSXIdentifier(path.node.name) && path.node.name.name !== 'svg') {
-          const attributes: (t.JSXAttribute | t.JSXSpreadAttribute)[] = [];
-          const className: t.Expression[] = [];
+export default (): { visitor: Visitor } => ({
+  visitor: {
+    JSXOpeningElement(path) {
+      if (t.isJSXIdentifier(path.node.name) && path.node.name.name !== 'svg') {
+        const attributes: (t.JSXAttribute | t.JSXSpreadAttribute)[] = [];
+        const className: t.Expression[] = [];
 
-          path.node.attributes.forEach(attribute => {
-            if (t.isJSXAttribute(attribute)) {
-              if (t.isJSXIdentifier(attribute.name)) {
-                if (attribute.name.name === 'className') {
-                  if (t.isJSXExpressionContainer(attribute.value)) {
-                    if (t.isExpression(attribute.value.expression)) {
-                      return className.push(attribute.value.expression);
-                    }
-                  }
-
-                  if (t.isStringLiteral(attribute.value)) {
-                    return className.push(attribute.value);
+        path.node.attributes.forEach(attribute => {
+          if (t.isJSXAttribute(attribute)) {
+            if (t.isJSXIdentifier(attribute.name)) {
+              if (attribute.name.name === 'className') {
+                if (t.isJSXExpressionContainer(attribute.value)) {
+                  if (t.isExpression(attribute.value.expression)) {
+                    return className.push(attribute.value.expression);
                   }
                 }
 
-                if (attribute.name.name in allowedJSXAttributes) {
-                  if (t.isJSXExpressionContainer(attribute.value)) {
-                    if (t.isExpression(attribute.value.expression)) {
-                      return className.push(
-                        t.callExpression(t.identifier('decodeResponsiveClassName'), [
-                          t.stringLiteral(allowedJSXAttributes[attribute.name.name as 'alignContent']),
-                          attribute.value.expression,
-                        ])
-                      );
-                    }
-                  }
+                if (t.isStringLiteral(attribute.value)) {
+                  return className.push(attribute.value);
+                }
+              }
 
-                  if (t.isStringLiteral(attribute.value)) {
+              if (attribute.name.name in allowedJSXAttributes) {
+                if (t.isJSXExpressionContainer(attribute.value)) {
+                  if (t.isExpression(attribute.value.expression)) {
                     return className.push(
                       t.callExpression(t.identifier('decodeResponsiveClassName'), [
                         t.stringLiteral(allowedJSXAttributes[attribute.name.name as 'alignContent']),
-                        attribute.value,
+                        attribute.value.expression,
                       ])
                     );
                   }
                 }
+
+                if (t.isStringLiteral(attribute.value)) {
+                  return className.push(
+                    t.callExpression(t.identifier('decodeResponsiveClassName'), [
+                      t.stringLiteral(allowedJSXAttributes[attribute.name.name as 'alignContent']),
+                      attribute.value,
+                    ])
+                  );
+                }
               }
             }
-
-            attributes.push(attribute);
-          });
-
-          if (className.length) {
-            attributes.push(
-              t.jsxAttribute(
-                t.jsxIdentifier('className'),
-                t.jsxExpressionContainer(
-                  t.callExpression(t.identifier('decodeClassName'), [t.arrayExpression(className)])
-                )
-              )
-            );
           }
 
-          path.node.attributes = attributes;
+          attributes.push(attribute);
+        });
+
+        if (className.length) {
+          attributes.push(
+            t.jsxAttribute(
+              t.jsxIdentifier('className'),
+              t.jsxExpressionContainer(
+                t.callExpression(t.identifier('decodeClassName'), [t.arrayExpression(className)])
+              )
+            )
+          );
         }
-      },
-      Program(path) {
-        path.unshiftContainer('body', [
-          t.importDeclaration(
-            [t.importDefaultSpecifier(t.identifier('decodeClassName'))],
-            t.stringLiteral('@warden-sk/babel-plugin/private/decodeClassName')
-          ),
-          t.importDeclaration(
-            [t.importDefaultSpecifier(t.identifier('decodeResponsiveClassName'))],
-            t.stringLiteral('@warden-sk/babel-plugin/private/decodeResponsiveClassName')
-          ),
-          t.importDeclaration([], t.stringLiteral('@warden-sk/design/public/index.css')),
-        ]);
-      },
+
+        path.node.attributes = attributes;
+      }
     },
-  };
-}
+    Program(path) {
+      path.unshiftContainer('body', [
+        t.importDeclaration(
+          [t.importDefaultSpecifier(t.identifier('decodeClassName'))],
+          t.stringLiteral('@warden-sk/babel-plugin/private/decodeClassName')
+        ),
+        t.importDeclaration(
+          [t.importDefaultSpecifier(t.identifier('decodeResponsiveClassName'))],
+          t.stringLiteral('@warden-sk/babel-plugin/private/decodeResponsiveClassName')
+        ),
+        t.importDeclaration([], t.stringLiteral('@warden-sk/design/public/index.css')),
+      ]);
+    },
+  },
+});
