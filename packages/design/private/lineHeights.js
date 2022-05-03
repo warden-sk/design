525e9bad960f0107f65487549a2d23802738d92e
Marek Kobida <marek.kobida@gmail.com>
2022-05-03 15:21:26 +0200

    updated design

---
+++ packages/design/private/lineHeights.js
@@ -0,0 +1,13 @@
+"use strict";
+/*
+ * Copyright 2022 Marek Kobida
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const lineHeights = [
+    ['1', '1'],
+    ['2', '1.25'],
+    ['3', '1.5'],
+    ['4', '1.75'],
+    ['5', '2rem'],
+];
+exports.default = lineHeights;
