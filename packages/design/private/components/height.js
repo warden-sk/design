5230fdc27436a9d431fe6371c88f18da80088262
Marek Kobida <marek.kobida@gmail.com>
2022-06-25 01:06:44 +0200

    update

---
+++ packages/design/private/components/height.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * Copyright 2022 Marek Kobida
+ */
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const allowedJSXAttributes_1 = __importDefault(require("../../../babel-plugin/private/allowedJSXAttributes"));
+const forBreakpoints_1 = __importDefault(require("../forBreakpoints"));
+function height() {
+    const $ = allowedJSXAttributes_1.default['height'];
+    return (0, forBreakpoints_1.default)(([b]) => ({
+        // .height-0
+        [`.${b}${$}0`]: { height: '0 !important' },
+        // .height-100
+        [`.${b}${$}100`]: { height: '100% !important' },
+        // .height-auto
+        [`.${b}${$}auto`]: { height: 'auto !important' },
+    }));
+}
+exports.default = height;
---
+++ packages/design/private/components/height.ts
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2022 Marek Kobida
+ */
+
+import type { CSS } from '../forBreakpoints';
+import allowedJSXAttributes from '../../../babel-plugin/private/allowedJSXAttributes';
+import forBreakpoints from '../forBreakpoints';
+
+function height(): CSS {
+  const $ = allowedJSXAttributes['height'];
+
+  return forBreakpoints(([b]) => ({
+    // .height-0
+    [`.${b}${$}0`]: { height: '0 !important' },
+    // .height-100
+    [`.${b}${$}100`]: { height: '100% !important' },
+    // .height-auto
+    [`.${b}${$}auto`]: { height: 'auto !important' },
+  }));
+}
+
+export default height;
