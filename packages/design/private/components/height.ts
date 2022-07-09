d5b21b72e154d36f869c47ae34fb98cc22aa8d39
Marek Kobida <marek.kobida@gmail.com>
2022-07-09 12:13:39 +0200

    update

--- packages/design/private/components/height.ts
+++ packages/design/private/components/height.ts
@@ -14,6 +14,18 @@ function height(): EnhancedCSSProperties {
     [`.${breakpointName}${$}0`]: {
       height: '0',
     },
+    // .height-25
+    [`.${breakpointName}${$}25`]: {
+      height: '25%',
+    },
+    // .height-50
+    [`.${breakpointName}${$}50`]: {
+      height: '50%',
+    },
+    // .height-75
+    [`.${breakpointName}${$}75`]: {
+      height: '75%',
+    },
     // .height-100
     [`.${breakpointName}${$}100`]: {
       height: '100%',
