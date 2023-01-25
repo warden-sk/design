88fee32fc5aaae5107b2ccc09405e1f47821a97a
Marek Kobida <marek.kobida@gmail.com>
2023-01-26 00:37:46 +0100

    update

--- packages/test/Test.tsx
+++ packages/test/Test.tsx
@@ -10,7 +10,7 @@ export function Test() {
   return <div p={isActive && '2'}>Test</div>;
 }
 
-export function Test1({ className }: JSX.IntrinsicElements['div']) {
+export function Test1({ className }: EnhancedJSXElement<'div'>) {
   return (
     <div className={[className, 'Test1']} p="2">
       Test1
@@ -18,18 +18,22 @@ export function Test1({ className }: JSX.IntrinsicElements['div']) {
   );
 }
 
-export function Test2($: JSX.IntrinsicElements['div']) {
+export function Test2({ p, ...$ }: EnhancedJSXElement<'div'>) {
   return (
-    <div {...$} className="Test2" p="2">
+    <div {...$} className="Test2" p={p}>
       Test2
     </div>
   );
 }
 
-export function Test3({ className, ...$ }: JSX.IntrinsicElements['div']) {
+export function Test3({ className, ...$ }: EnhancedJSXElement<'div'>) {
   return (
     <div {...$} className={[className, 'Test3']} p="2">
       Test3
     </div>
   );
+}
+
+export function Component() {
+  return <Test2 className="Component" id="2" p="4" />;
 }
