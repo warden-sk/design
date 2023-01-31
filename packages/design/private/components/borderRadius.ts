89f7a28052bf4cd0fe5cf74d5d2ad0570a749168
Marek Kobida <marek.kobida@gmail.com>
2023-01-31 05:04:28 +0100

    update

--- packages/design/private/components/borderRadius.ts
+++ packages/design/private/components/borderRadius.ts
@@ -15,6 +15,10 @@ const sizes = [
 
 function borderRadius(): EnhancedCSSProperties {
   const $ = dictionary.get('borderRadius');
+  const b = dictionary.get('borderBottomRadius');
+  const l = dictionary.get('borderLeftRadius');
+  const r = dictionary.get('borderRightRadius');
+  const t = dictionary.get('borderTopRadius');
 
   return forBreakpoints(([breakpointName]) => {
     return {
@@ -23,6 +27,22 @@ function borderRadius(): EnhancedCSSProperties {
           ..._,
           [`.${breakpointName}${$}${dictionary.get(left)}`]: {
             borderRadius: right,
+          },
+          [`.${breakpointName}${b}${dictionary.get(left)}`]: {
+            borderBottomLeftRadius: right,
+            borderBottomRightRadius: right,
+          },
+          [`.${breakpointName}${l}${dictionary.get(left)}`]: {
+            borderBottomLeftRadius: right,
+            borderTopLeftRadius: right,
+          },
+          [`.${breakpointName}${r}${dictionary.get(left)}`]: {
+            borderBottomRightRadius: right,
+            borderTopRightRadius: right,
+          },
+          [`.${breakpointName}${t}${dictionary.get(left)}`]: {
+            borderTopLeftRadius: right,
+            borderTopRightRadius: right,
           },
         }),
         {}
